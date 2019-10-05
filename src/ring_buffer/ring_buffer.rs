a4ba96616f7521a2f9b0bd3fb42e314e62c8e38c
neetdai <ake17101@gmail.com>
2019-10-05 22:18:24 +0800

    尝试修改为无锁结构

--- src/ring_buffer/ring_buffer.rs
+++ src/ring_buffer/ring_buffer.rs
@@ -1,6 +1,8 @@
-use std::cell::UnsafeCell;
 use std::cmp::min;
 use std::fmt::{Debug, Error as FmtError, Formatter};
+use std::marker::PhantomData;
+use std::mem;
+use std::ops::Drop;
 use std::ops::Fn;
 use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 use std::usize::MAX;
@@ -17,23 +19,29 @@ impl Debug for Error {
     }
 }
 
+struct Inner(*mut u8);
+
+unsafe impl Sync for Inner {}
+
 pub struct RingBuffer {
-    inner: *mut u8,
+    inner: Inner,
     size: usize,
     lock: AtomicBool,
     write_position: AtomicUsize,
     read_position: AtomicUsize,
-    callback: Option<Box<dyn Fn(Vec<u8>) + 'static>>,
+    callback: Option<Box<dyn Fn(Vec<u8>) + 'static + Sync>>,
 }
 
 impl RingBuffer {
     pub fn new(size: usize) -> Self {
         RingBuffer {
-            inner: {
+            inner: Inner({
                 let mut buffer: Vec<u8> = vec![0; size];
-                buffer.as_mut_ptr()
-            },
-            size: size,
+                let ptr: *mut u8 = buffer.as_mut_ptr();
+                mem::forget(buffer);
+                ptr
+            }),
+            size,
             lock: AtomicBool::new(false),
             write_position: AtomicUsize::new(0),
             read_position: AtomicUsize::new(0),
@@ -102,7 +110,7 @@ impl RingBuffer {
 
     pub fn write(&self, buf: &[u8]) -> usize {
         loop {
-            if self.lock.compare_and_swap(false, true, Ordering::AcqRel) == false {
+            if !self.lock.compare_and_swap(false, true, Ordering::AcqRel) {
                 if self.is_full() {
                     self.lock.store(false, Ordering::Release);
                     return 0;
@@ -116,11 +124,12 @@ impl RingBuffer {
                     .iter()
                     .enumerate()
                     .for_each(|(index, item)| {
-                        let tmp: *mut u8 =
-                            unsafe { self.inner.add((index + write_position) % inner_len) };
                         unsafe {
-                            tmp.write(*item);
-                        };
+                            self.inner
+                                .0
+                                .offset(((index + write_position) % inner_len) as isize)
+                                .write(*item);
+                        }
                         self.write_position.store(
                             (self.write_position.load(Ordering::Acquire) + 1) % MAX,
                             Ordering::Release,
@@ -135,20 +144,25 @@ impl RingBuffer {
 
     pub fn read_all(&self) -> Vec<u8> {
         loop {
-            if self.lock.compare_and_swap(false, true, Ordering::AcqRel) == false {
+            if !self.lock.compare_and_swap(false, true, Ordering::AcqRel) {
                 let avaliable_read_len: usize = self.avaliable_read_len();
                 let read_position: usize = self.read_position.load(Ordering::Acquire);
                 let mut result: Vec<u8> = Vec::with_capacity(avaliable_read_len);
                 let inner_len: usize = self.size;
 
                 (read_position..(read_position + avaliable_read_len)).for_each(|index| {
-                    result.push(unsafe { self.inner.add((index) % inner_len) as u8 });
+                    result.push(unsafe {
+                        // let tmp: *mut u8 = self.inner.add((index) % inner_len) as *mut u8;
+                        // *tmp
+                        self.inner.0.offset(((index) % inner_len) as isize).read()
+                    });
                     self.read_position.store(
                         (self.read_position.load(Ordering::Acquire) + 1) % MAX,
                         Ordering::Release,
                     );
                 });
 
+                self.lock.store(false, Ordering::Release);
                 return result;
             }
         }
@@ -156,7 +170,7 @@ impl RingBuffer {
 
     pub fn set_callback<T>(&mut self, fnc: T)
     where
-        T: Fn(Vec<u8>) + 'static,
+        T: Fn(Vec<u8>) + 'static + Sync,
     {
         self.callback = Some(Box::new(fnc));
     }
@@ -175,9 +189,9 @@ impl RingBuffer {
                 start += self.write(&buf[start..]);
                 let result: Vec<u8> = self.read_all();
                 if self.is_full() {
-                    self.callback.as_mut().map(|fnc| {
+                    if let Some(fnc) = self.callback.as_mut() {
                         fnc(result);
-                    });
+                    }
                 }
             }
 
@@ -188,12 +202,29 @@ impl RingBuffer {
 
 impl Debug for RingBuffer {
     fn fmt(&self, f: &mut Formatter) -> Result<(), FmtError> {
-
         f.debug_struct("RingBuffer")
-            .field("inner", &self.inner)
-            .field("write_position", &self.write_position.load(Ordering::Acquire))
+            .field("inner", unsafe { &self.inner.0.read() })
+            .field(
+                "write_position",
+                &self.write_position.load(Ordering::Acquire),
+            )
             .field("read_position", &self.read_position.load(Ordering::Acquire))
             .field("size", &self.size)
             .finish()
     }
 }
+
+impl Drop for RingBuffer {
+    fn drop(&mut self) {
+        (0..self.size).for_each(|index| unsafe {
+            self.inner.0.add(index).drop_in_place();
+        });
+
+        unsafe {
+            Vec::from_raw_parts(self.inner.0, 0, self.size);
+        }
+    }
+}
+
+unsafe impl Sync for RingBuffer {}
+unsafe impl Send for RingBuffer {}
--- tests/ring_buffer_test.rs
+++ tests/ring_buffer_test.rs
@@ -71,3 +71,91 @@ fn test_6() {
 
     rb.callback_by_write(&buf).unwrap();
 }
+
+#[test]
+fn test_7() {
+    use std::sync::Arc;
+    use std::thread::{sleep_ms, spawn};
+
+    let rb = Arc::new(RingBuffer::new(16));
+
+    let tmp = rb.clone();
+    let j1 = spawn(move || {
+        let buffer = [2; 9];
+        assert_eq!(tmp.write(&buffer), 9);
+    });
+
+    let tmp2 = rb.clone();
+    let j2 = spawn(move || {
+        sleep_ms(100);
+        let buffer = [1; 9];
+        assert_eq!(tmp2.write(&buffer), 7);
+    });
+
+    j1.join().unwrap();
+    j2.join().unwrap();
+
+    assert_eq!(
+        rb.read_all(),
+        vec![2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1]
+    );
+}
+
+#[test]
+fn test_8() {
+    use std::sync::Arc;
+    use std::thread::spawn;
+
+    let rb = Arc::new(RingBuffer::new(16));
+
+    let tmp1 = rb.clone();
+    let tmp2 = rb.clone();
+
+    let j1 = spawn(move || {
+        let buffer = [1; 9];
+        assert_eq!(tmp1.write(&buffer), 9);
+    });
+
+    let j2 = spawn(move || {
+        loop {
+            let result = tmp2.read_all();
+            if result.len() > 0 {
+                assert_eq!(result.len(), 9);
+                assert_eq!(result, vec![1; 9]);
+                break;
+            }
+        }
+    });
+
+    j1.join().unwrap();
+    j2.join().unwrap();
+}
+
+#[test]
+fn test_9() {
+    use std::sync::Arc;
+    use std::thread::{spawn, sleep_ms};
+
+    let rb = Arc::new(RingBuffer::new(16));
+    
+    let tmp1 = rb.clone();
+    let tmp2 = rb.clone();
+    let tmp3 = rb.clone();
+
+    let j1 = spawn(move || {
+        assert_eq!(tmp1.write(&[1; 9]), 9);
+    });
+
+    let j2 = spawn(move || {
+        assert_eq!(tmp2.write(&[2; 9]), 7);
+    });
+
+    let j3 = spawn(move || {
+        let result = tmp3.read_all();
+        assert!(result == vec![1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2] || result == vec![2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1] || result.is_empty());
+    });
+
+    j1.join();
+    j2.join();
+    j3.join();
+}
